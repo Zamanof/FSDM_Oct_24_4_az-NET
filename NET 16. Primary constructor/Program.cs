df4ae32e0f3298c464a81999b55623f275fbeb7b
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-06-22 11:04:15 +0400

    primary constructor

---
+++ NET 16. Primary constructor/Program.cs
@@ -0,0 +1,44 @@
+﻿// primary constructor -> new in C#12
+
+#region classic constructor
+//Cat cat = new("Firon", 2500, 0, "Sfinks");
+//Console.WriteLine(cat);
+
+//class Cat
+//{
+//    string name;
+//    int age;
+//    int lifeCount;
+//    string breed;
+
+//    public Cat(string name, int age, int lifeCount, string breed)
+//    {
+//        this.name = name;
+//        this.age = age;
+//        this.lifeCount = lifeCount;
+//        this.breed = breed;
+//    }
+
+//    public override string ToString()
+//    {
+//        return $"Name: {name}, Age: {age}, Life Count: {lifeCount}, Gender: {breed}";
+//    }
+//} 
+#endregion
+
+
+#region primary constructor
+//Cat cat = new("Firon", 2500, 0, "Sfinks");
+//Console.WriteLine(cat);
+
+//class Cat(string name, int age, int lifeCount, string breed)
+//{
+//    public override string ToString()
+//    {
+//        return $"Name: {name}, Age: {age}, Life Count: {lifeCount}, Gender: {breed}";
+//    }
+//}
+#endregion
+
+
+
